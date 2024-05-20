3427e4a6ceea2ddb738a237d03c044c89cf76d53
Nlucky320 <luckyes144@gmail.com>
2024-05-21 00:18:23 +0600

    server added

--- bistro-boss-client/src/Pages/Order/OrderTab.jsx
+++ bistro-boss-client/src/Pages/Order/OrderTab.jsx
@@ -4,7 +4,7 @@ import FoodCard from '../../component/Shared/FoodCard';
 const OrderTab = ({items}) => {
     return (
         <div>
-              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                               {
                             items.map(item=><FoodCard key={item._id} item={item}></FoodCard>)
                             }
--- bistro-boss-client/src/component/Shared/FoodCard.jsx
+++ bistro-boss-client/src/component/Shared/FoodCard.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 const FoodCard = ({ item }) => {
     const { name, image, price, recipe } = item;
     return (
-       <div className="card  bg-base-100 shadow-xl">
-  <figure className="p-4">
-    <img src={image} alt="food" className="rounded-xl" />
+       <div className="card  bg-[#F3F3F3] shadow-xl mt-4">
+  <figure className="">
+    <img src={image} alt="food" className="" />
             </figure>
             <p className='bg-[#BB8506] text-white absolute right-0 mr-4 mt-4 px-4'>${price}</p>
-  <div className="card-body items-center text-center">
+  <div className="card-body  text-center flex flex-col items-center" >
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
     <div className="card-actions">
-      <button className="btn text-[#BB8506] bg-black hover:bg-white border-0 border-b-2 border-[#BB8506]">Add To Cart</button>
+      <button className="btn text-[#BB8506] hover:bg-black bg-[#E8E8E8] border-0 border-b-2 border-[#BB8506]">Add To Cart</button>
     </div>
   </div>
 </div>
