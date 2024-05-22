cc7e521b6e480087f2975dc07eba1d5e0a57f626
Nlucky320 <luckyes144@gmail.com>
2024-05-22 19:19:08 +0600

    sign up page updated

--- bistro-boss-client/src/AuthProvider/AuthProvider.jsx
+++ bistro-boss-client/src/AuthProvider/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 import { app } from '../Firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -24,7 +24,17 @@ const AuthProvider = ({ children }) => {
         setLoading(true);
         return signOut(auth);
     };
-
+    const updateUserProfile = (name, photo) => {
+return updateProfile(auth.currentUser, {
+  displayName: name, photoURL: photo
+}).then(() => {
+  // Profile updated!
+  // ...
+}).catch((error) => {
+  // An error occurred
+  // ...
+});
+}
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
@@ -42,6 +52,7 @@ const AuthProvider = ({ children }) => {
         logOut,
         user,
         loading,
+        updateUserProfile ,
       
     };
 
--- bistro-boss-client/src/Pages/SignUp/SignUp.jsx
+++ bistro-boss-client/src/Pages/SignUp/SignUp.jsx
@@ -1,74 +1,147 @@
-import React, { useContext } from 'react';
-import { Helmet } from 'react-helmet-async';
-import { useForm } from "react-hook-form"
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../AuthProvider/AuthProvider';
+import React, { useContext } from "react";
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import Swal from "sweetalert2";
 const SignUp = () => {
-const {createUser}=useContext(AuthContext)
+  const { createUser, updateUserProfile } = useContext(AuthContext);
+const navigate=useNavigate()
+  const {
+    register,
+      handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    createUser(data.email, data.password).then((result) => {
+      const loggedUser = result.user;
+        console.log(loggedUser);
+        updateUserProfile(data.name, data.photoURL)
+            .then(() => {
+                console.log('user profile updated')
+                reset();
+                Swal.fire({
+  position: "top-end",
+  icon: "success",
+  title: "User created successfully",
+  showConfirmButton: false,
+  timer: 1500
+                });
+                navigate('/')
 
-    const onSubmit = (data) => {
-        console.log(data)
-        createUser(data.email, data.password)
-            .then(result => {
-                const loggedUser = result.user;
-                console.log(loggedUser)
-        })
-  }
-    return (
-        <>
-            <Helmet>
-                <title>Bistro Boss | Sign Up</title>
-            </Helmet>
-             <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Signup now!</h1>
-    </div>
-    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Name</span>
-          </label>
-                            <input type="text" {...register("name",  { required: true })} placeholder="name" className="input input-bordered" />
-                                 {errors.name && <p className="text-red-600">This field is required</p>}
+            })
+        .catch(error=>console.log(error))
+    });
+  };
+  return (
+    <>
+      <Helmet>
+        <title>Bistro Boss | Sign Up</title>
+      </Helmet>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Signup now!</h1>
+          </div>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("name", { required: true })}
+                  placeholder="name"
+                  className="input input-bordered"
+                />
+                {errors.name && (
+                  <p className="text-red-600">This field is required</p>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">photo URL</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("photo", { required: true })}
+                  placeholder="Photo"
+                  className="input input-bordered"
+                />
+                {errors.photo && (
+                  <p className="text-red-600">This field is required</p>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  {...register("email", { required: true })}
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+                {errors.email && (
+                  <p className="text-red-600">This field is required</p>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  {...register("password", {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 20,
+                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                  })}
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                {errors.password?.type === "required" && (
+                  <p className="text-red-600">Password is required</p>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <p className="text-red-600">Password must be 6 characters</p>
+                )}
+                {errors.password?.type === "maxLength" && (
+                  <p className="text-red-600">
+                    Password must be less than 20 characters
+                  </p>
+                )}
+                {errors.password?.type === "pattern" && (
+                  <p className="text-red-600">
+                    Password must have one Uppercase one lower case, one number
+                    and one special character.
+                  </p>
+                )}
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn bg-[#D1A054] text-white">Sign Up</button>
+              </div>
+            </form>
+            <p className="px-4 pb-4">
+              <small>
+                Already have an account? please <Link to="/login"> Login</Link>
+              </small>
+            </p>
+          </div>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-                            <input type="email"  {...register("email",  { required: true })} placeholder="email" className="input input-bordered"  />
-                                 {errors.email && <p className="text-red-600">This field is required</p>}
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-                            <input type="password"  {...register("password",  { required: true, minLength:6, maxLength: 20,  pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/  })} placeholder="password" className="input input-bordered"  />
-                              {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
-                                {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
-                                {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters</p>}
-                                {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase one lower case, one number and one special character.</p>}
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn bg-[#D1A054] text-white">Sign Up</button>
-        </div>
-                    </form>
-                        <p className='px-4 pb-4'><small>Already have an account? please <Link to='/login'> Login</Link></small></p>
-    </div>
-  </div>
-            </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default SignUp;
