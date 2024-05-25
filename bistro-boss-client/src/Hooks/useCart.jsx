e3e3dff3558d38b9bdaebf16db86b5bcbf25b6c2
Nlucky320 <luckyes144@gmail.com>
2024-05-26 00:58:52 +0600

    verification added

--- bistro-boss-client/src/Hooks/useCart.jsx
+++ bistro-boss-client/src/Hooks/useCart.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import useAxiosSecure, { axiosSecure } from "./useAxiosSecure";
+import useAxiosSecure from "./useAxiosSecure";
 import useAuth from "./useAuth";
 
 
--- bistro-boss-server/index.js
+++ bistro-boss-server/index.js
@@ -40,6 +40,22 @@ async function run() {
       res.send({ token });
     });
 
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
     //user collection
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -54,7 +70,8 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyToken, async (req, res) => {
+      // console.log(req.headers);
       const result = await userCollection.find().toArray();
       res.send(result);
     });
