86f862dc533e283f355bd97f6b7a21f424dc7145
Nlucky320 <luckyes144@gmail.com>
2024-05-29 19:22:04 +0600

    stripe payment added

--- bistro-boss-client/src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ bistro-boss-client/src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,39 +1,85 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useCart from '../../../Hooks/useCart';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
+import useAuth from '../../../Hooks/useAuth';
 
 const CheckoutForm = () => {
+    const [error, setError] = useState('');
+    const [clientSecret, setClientSecret] = useState('')
+    const axiosSecure = useAxiosSecure();
+    const [cart] = useCart();
     const stripe = useStripe();
-      const elements = useElements();
+    const elements = useElements();
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
+    const { user } = useAuth();
+    const [transactionId, setTransactionId] = useState('');
+    useEffect(() => {
+        if (totalPrice > 0) {
+            axiosSecure.post('/create-payment-intent', { price: totalPrice })
+                .then(res => {
+                    // console.log(res.data.clientSecret);
+                    setClientSecret(res.data.clientSecret);
+                })
+        }
+
+    }, [axiosSecure, totalPrice])
+
     const handleSubmit = async (event) => {
         event.preventDefault();
- if (!stripe || !elements) {
-      // Stripe.js has not loaded yet. Make sure to disable
-      // form submission until Stripe.js has loaded.
-      return;
+        if (!stripe || !elements) {
+            // Stripe.js has not loaded yet. Make sure to disable
+            // form submission until Stripe.js has loaded.
+            return;
         }
         
-           // Get a reference to a mounted CardElement. Elements knows how
-    // to find your CardElement because there can only ever be one of
-    // each type of element.
-    const card = elements.getElement(CardElement);
+        // Get a reference to a mounted CardElement. Elements knows how
+        // to find your CardElement because there can only ever be one of
+        // each type of element.
+        const card = elements.getElement(CardElement);
 
-    if (card == null) {
-      return;
+        if (card == null) {
+            return;
         }
         
-    // Use your card Element with other Stripe.js APIs
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
-      type: 'card',
-      card,
-    });
+        // Use your card Element with other Stripe.js APIs
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
 
-    if (error) {
-      console.log('[error]', error);
-    } else {
-      console.log('[PaymentMethod]', paymentMethod);
+        if (error) {
+            console.log('[error]', error);
+            setError(error.message);
+        } else {
+            console.log('[PaymentMethod]', paymentMethod);
+            setError('')
+        }
+        
+
+        //confirm payment
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    email: user?.email || 'anonymous',
+                    name: user?.displayName || 'anonymous',
+                }
+            }
+        })
+        if (confirmError) {
+            console.log('confirm error')
+        }
+        else {
+            console.log('payment intent', paymentIntent)
+            if (paymentIntent.status === 'succeeded') {
+                console.log('transaction id', paymentIntent.id);
+                setTransactionId(paymentIntent.id);
+
+            }
+        }
     }
-    }
-    
+
     return (
         <form onSubmit={handleSubmit}>
           <CardElement
@@ -53,10 +99,13 @@ const CheckoutForm = () => {
         }}
       />
             <div className='flex justify-center pt-4'>
-                   <button type="submit" className='btn btn-sm px-20 items-center bg-[#570DF8] text-center text-white' disabled={!stripe}>
+                   <button type="submit" className='btn btn-sm px-20 items-center bg-[#570DF8] text-center text-white' disabled={!stripe || !clientSecret}>
         Pay
-      </button>
-   </div>
+                </button>
+                 
+            </div>
+            <p className="text-red-600">{error}</p>
+            {transactionId && <p className="text-green-600"> Your transaction id: {transactionId}</p>}
        </form>
     );
 };
--- bistro-boss-server/index.js
+++ bistro-boss-server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 //middleware
 
@@ -192,6 +193,23 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
+    });
+
+    // payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      // console.log(amount, "amount inside the intent");
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     // Send a ping to confirm a successful connection
